91b95466395521ad7602288c7c99a889bb736755
David Evans <davidje13@users.noreply.github.com>
2018-07-31 21:29:56 +0100

    Experiments with Titanic data

--- launch.sh
+++ launch.sh
@@ -4,5 +4,5 @@ BASEDIR="$(cd "$(dirname "$0")"; pwd)";
 
 docker run -it \
   -p 8888:8888 \
-  -v "$BASEDIR":/notebooks/persisted \
+  -v "$BASEDIR":/notebooks \
   tensorflow/tensorflow:latest-py3
